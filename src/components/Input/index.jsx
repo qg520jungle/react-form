6e90a9bcef4e01eb2182f10e7dba28b53a7f9cab
zhangxianggeng@wuxicloud.com <zhangxianggeng@wuxicloud.com>
2020-07-03 01:22:41 +0800

    提交 input 的建议封装

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,26 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 function Input(props) {
+  useEffect(() => {
+    console.log(props);
+  }, [props]);
+  // const {onChange} = props
+  const onChange = (e) => {
+    console.log('input', e.target.value);
+    props.onChange(e.target.value);
+  };
   return (
     <div className="page">
-      <input {...props} />
+      <input {...props} onChange={onChange} />
     </div>
   );
 }
+
+Input.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default Input;
--- src/views/FormPage/index.jsx
+++ src/views/FormPage/index.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'components/Form';
 import Input from 'components/Input';
-// import Select from 'components/Select';
-// import Checkbox from 'components/Checkbox';
+// // import Select from 'components/Select';
+// // import Checkbox from 'components/Checkbox';
 
 function FormPage() {
+  const [value, setValue] = useState('');
+  function formSubmit() {
+    console.log(value);
+  }
+  function onInputChange(e) {
+    console.log('before input', e);
+    setValue(e);
+  }
   return (
     <div className="page">
       <Form>
-        <Input />
-        {/* <Select />
-        <Checkbox /> */}
+        <Input type="text" value={value} onChange={onInputChange} />
+        <button type="button" onClick={formSubmit}>
+          提交
+        </button>
       </Form>
     </div>
   );
